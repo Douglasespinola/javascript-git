f6fa7adc5a7ab50d5341ded9d9bcd1e50e159ff4
Douglas Espínola <douglasespinola8@gmail.com>
2024-02-23 08:31:16 -0300

    tabnav refatorado em class

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import initScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
-import initTabNav from "./modules/tabnav.js";
+import TabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
@@ -13,9 +13,14 @@ import initAnimacaoScroll from "./modules/scroll-animacao.js";
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
+);
+tabNav.init();
+
 initFetchAnimais();
 initScrollSuave();
-initTabNav();
 initModal();
 initTooltip();
 initDropdownMenu();
